52373197b197cd70c06177794b45136642d912a3
Marina <gtellier.mecanics@gmail.com>
2022-07-24 18:03:49 +0300

    Строка с наименьшей суммой элементов

---
+++ task2/Program.cs
@@ -0,0 +1,58 @@
+﻿int rows = 4;
+int columns = 4;
+int[,] array = new int[rows, columns];
+FillArray(array);
+PrintArray(array);
+FindingSum(array);
+
+void FindingSum(int[,] array)
+{
+    int sumMin = 0;
+    int sumMinIndex = 0;
+    for (int j = 0; j < columns; j++)
+    {
+        sumMin += array[0, j];
+    }
+   
+    for (int i = 1; i<rows; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j<columns; j++)
+        {
+            sum += array[i,j];
+        }
+
+        if (sum < sumMin)
+        {
+            sumMin = sum;
+            sumMinIndex = i;
+        }
+    }
+
+    Console.WriteLine($"Номер строки с наименьшей суммой элементов: {sumMinIndex+1}");
+
+}
+
+void FillArray(int[,] array)
+{
+    Random random = new Random();
+    for (int i = 0; i<rows; i++)
+{
+    for (int j = 0; j<columns; j++)
+    {
+        array[i,j] = random.Next(1, 10);
+    }
+}
+}
+
+void PrintArray(int[,] array)
+{
+for (int i = 0; i<rows; i++)
+{
+    for (int j = 0; j<columns; j++)
+    {
+        Console.Write(array[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
+}
