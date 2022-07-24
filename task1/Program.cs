36f95c78a13fa57b55966a03c0dd899d537515d2
Marina <gtellier.mecanics@gmail.com>
2022-07-24 17:37:15 +0300

    Сортировка строк

---
+++ task1/Program.cs
@@ -0,0 +1,54 @@
+﻿int rows = 3;
+int columns = 4;
+int[,] array = new int[rows, columns];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine("Отсортированный массив:");
+Sorting(array);
+PrintArray(array);
+
+
+
+
+void Sorting(int[,] array)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            for (int k = 0; k < columns - j - 1; k++)
+            {
+                if (array[i,k] < array[i, k+1])
+                {
+                    int temp = array[i,k];
+                    array[i,k] = array[i, k+1];
+                    array[i, k+1] = temp;
+                }
+            }
+        }
+    }
+}
+
+void FillArray(int[,] array)
+{
+    Random random = new Random();
+    for (int i = 0; i<rows; i++)
+{
+    for (int j = 0; j<columns; j++)
+    {
+        array[i,j] = random.Next(1, 10);
+    }
+}
+}
+
+void PrintArray(int[,] array)
+{
+for (int i = 0; i<rows; i++)
+{
+    for (int j = 0; j<columns; j++)
+    {
+        Console.Write(array[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
+}
